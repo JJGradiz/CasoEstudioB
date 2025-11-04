9f0403e0f9312d2163be1bfebb2cdc11c463f345
JJGradiz <jjgradiz@uamv.edu.ni>
2025-11-04 09:27:51 -0600

    Codigo de ejemplo del caso de estudio

--- CasoEstudioB/Program.cs
+++ CasoEstudioB/Program.cs
@@ -6,6 +6,8 @@ using System.Threading.Tasks;
 
 namespace CasoEstudioB
 {
+    //Este es un ejemplo de como se debe ir trabajando la MiniApp 
+    // de nuestro Caso de Estudio B
     internal class Program
     {
         static void Main(string[] args)
