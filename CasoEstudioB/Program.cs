0cbd5a85a2015e745c38f6060cb5cbf33ccdc7e1
Faza202 <fancajima@uamv.edu.ni>
2025-11-04 09:20:14 -0600

    se agregó las clases y un método para llamarlas

--- CasoEstudioB/Program.cs
+++ CasoEstudioB/Program.cs
@@ -6,10 +6,41 @@ using System.Threading.Tasks;
 
 namespace CasoEstudioB
 {
+    class Curso
+    {
+        public int Id { get; }
+        public string Nombre { get; }
+        public string Area { get; }
+
+        public Curso(int id, string nombre, string area)
+        {
+            Id = id;
+            Nombre = nombre ?? throw new ArgumentNullException(nameof(nombre));
+            Area = area ?? throw new ArgumentNullException(nameof(area));
+        }
+
+        public override string ToString()
+        {
+            return $"{Id}: {Nombre} ({Area})";
+        }
+    }
+
     internal class Program
     {
         static void Main(string[] args)
         {
+            var cursos = new List<Curso>
+            {
+                new Curso(1, "Programación Estructurada", "Informática"),
+                new Curso(2, "Robótica", "Ingeniería"),
+                new Curso(3, "Análisis y Diseño", "Software"),
+                new Curso(4, "Cálculo", "Matemáticas")
+            };
+
+            foreach (var curso in cursos)
+            {
+                Console.WriteLine(curso);
+            }
         }
     }
 }
